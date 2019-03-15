99bc4f8ad3454a4a4ce61a8e28e7a0b53e9b2735
sanjayparidar <sanjaypatidar402@gmail.com>
2019-03-15 14:11:54 +0530

    second

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express=require("express");
 var app=express();
 var path = require('path');
-app.use(express.static(__dirname+'/dist/'));
+app.use(express.static(__dirname+'/dist/NewPro'));
 
 app.get("/*",function(req,res){
-    res.sendFile(path.join(__dirname+"/dist"));
+    res.sendFile(path.join(__dirname+"/dist/NewPro/index.html"));
 });
 app.listen(process.env.PORT|| 3000,function(){
     console.log('server')
